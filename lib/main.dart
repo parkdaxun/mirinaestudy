6e87d55b00f053b35ba201627f5826011d98478d
parkdaeun <parkde0207@gmail.com>
2024-08-19 15:02:18 +0900

    🔨 :: Splash Screen 개발 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:mirinaestudy/screen/home_screen.dart';
-import 'package:mirinaestudy/screen/mypage_screen.dart';
-import 'package:mirinaestudy/widget/bottom_bar.dart';
-
+import 'package:mirinaestudy/screen/splash_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mirinae Study',
-      theme: ThemeData(
-        primaryColor: Color(0xff5F2FA8),
-      ),
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          backgroundColor: Colors.white,
-          body: TabBarView(
-            physics: NeverScrollableScrollPhysics(),
-            children: <Widget>[
-              HomeScreen(),
-              MypageScreen(),
-            ],
-          ),
-          bottomNavigationBar: BottomBar(),
-        )
-      )
+      title: 'mirinea study',
+      home: SplashScreen(),
     );
   }
 }
---
+++ lib/screen/splash_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:mirinaestudy/screen/account_select_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToAccountSelectScreen();
+  }
+
+  _navigateToAccountSelectScreen() async {
+    await Future.delayed(Duration(milliseconds: 3000), () {});
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => AccountSelectScreen()),);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff3BA2F9),
+      body: Center(
+        child: Image.asset('assets/images/mirinaestudy_white_logo.png'),
+      ),
+    );
+  }
+}
+
