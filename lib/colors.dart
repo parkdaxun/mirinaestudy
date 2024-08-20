62187bc6173a61f1e2313007e84565b1986603e6
parkdaeun <parkde0207@gmail.com>
2024-08-20 16:04:09 +0900

    🔨 :: 색 수정

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color purple = Color(0xff5F2FA8);
-  static const Color grey = Color(0xffA6A6A6);
+  static const Color blue = Color(0xff3BA2F9);
+  static const Color darkBlue = Color(0xff21469C);
   static const Color green = Color(0xffDAED58);
-  static const Color lightPurple = Color(0xffC67FDC);
+  static const Color lightBlue = Color(0xff8DEAFF);
   static const Color fillBackground = Color(0xffF7F7F7);
 }
