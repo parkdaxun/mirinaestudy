91ec007c10578ca76b772d678b95d1165ca4fcc9
parkdaxun <parkde0207@gmail.com>
2024-09-04 18:16:19 +0900

    💄 :: font 추가, WelcomeScreen 적용

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'NotoSansKR',
       ),
       title: 'mirinea study',
       home: SplashScreen(),
