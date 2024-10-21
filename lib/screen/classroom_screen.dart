20393afbe3f3119a2c1efd0c1ec09c1497714473
parkdaxun <parkde0207@gmail.com>
2024-10-21 18:28:52 +0900

    FIX :: ClassroomScreen 코드 정리

--- lib/screen/classroom_screen.dart
+++ lib/screen/classroom_screen.dart
@@ -4,40 +4,97 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mirinaestudy/widget/app_bar.dart';
 
 import '../colors.dart';
-import '../widget/classroom_post_widget.dart';
+import '../widget/classroom/classroom_post_widget.dart';
 
 class ClassroomScreen extends StatefulWidget {
   _ClassroomScreenState createState() => _ClassroomScreenState();
 }
 
+class PostList {
+  String name;
+  String date;
+  String content;
+  int like;
+  int comment;
+
+  PostList({
+    required this.name,
+    required this.date,
+    required this.content,
+    required this.like,
+    required this.comment,
+  });
+}
+
 class _ClassroomScreenState extends State<ClassroomScreen> {
+  final List<PostList> posts = [
+    PostList(
+      name: 'Park Daeun',
+      date: '2024.08.30 오후 02:28',
+      content: 'This paper investigates the cognitive processes involved in English word recognition among young EFL learners using eye-tracking methodology. A quasi-experimental mixed method design was used to investigate how young.',
+      like: 3,
+      comment: 5,
+    ),
+    PostList(
+      name: 'Park Daeun',
+      date: '2024.08.30 오후 02:28',
+      content: '안녕하세요',
+      like: 3,
+      comment: 5,
+    ),
+    PostList(
+      name: 'Park Daeun',
+      date: '2024.08.30 오후 02:28',
+      content: '안녕하세요',
+      like: 3,
+      comment: 5,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.fillGrey,
       appBar: CustomAppBar(title: '교실'),
-      body: Center(
-        child: Column(
+      body: Column(
           children: [
-            Container(
-              width: MediaQuery.of(context).size.width*0.91,
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 26, bottom: 13),
-                    child: AddClassWidget(),
-                  ),
-                  ClassroomPostWidget(
-                    name: '',
-                    date: '',
-                    like: 1,
-                    comment: 1,
-                    content: '',
-                  ),
-                ],
+            Padding(
+              padding: const EdgeInsets.only(top: 26, bottom: 13),
+              child: AddClassWidget(),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 13),
+                child: postList(),
               ),
             ),
           ],
+      ),
+    );
+  }
+
+  Widget postList() {
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Center(
+      child: SizedBox(
+        width: screenWidth*0.91,
+        child: ListView.builder(
+          physics: BouncingScrollPhysics(),
+          itemBuilder: (context, index) {
+            final postList = posts[index];
+            return Padding(
+              padding: const EdgeInsets.only(bottom: 11),
+              child: ClassroomPostWidget(
+                  name: postList.name,
+                  date: postList.date,
+                  content: postList.content,
+                  like: postList.like,
+                  comment: postList.comment,
+              ),
+            );
+          },
+          itemCount: posts.length,
         ),
       ),
     );
@@ -45,6 +102,7 @@ class _ClassroomScreenState extends State<ClassroomScreen> {
 
   Widget AddClassWidget() {
     return Container(
+      width: MediaQuery.of(context).size.width*0.91,
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(10),
